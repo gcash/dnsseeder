4f28b694a4d15a26c0c0c56f0bce8b95b3c8a913
Chris Pacia <chris@ob1.io>
2018-10-29 20:27:02 -0400

    Remove duplicate network magic check

--- seeder.go
+++ seeder.go
@@ -544,9 +544,6 @@ func isDuplicateSeeder(s *dnsseeder) (bool, error) {
 
 	// check for duplicate seeders with the same details
 	for _, v := range config.seeders {
-		if v.id == s.id {
-			return true, fmt.Errorf("Duplicate Magic id. Already loaded for %s so can not be used for %s", v.id, v.name, s.name)
-		}
 		if v.dnsHost == s.dnsHost {
 			return true, fmt.Errorf("Duplicate DNS names. Already loaded %s for %s so can not be used for %s", v.dnsHost, v.name, s.name)
 		}
